00049fd1f0e94858ab604812773fa3d85a5fd825
Ghost-Rider <ghostrider.gu@gmail.com>
2020-05-11 22:57:37 +0300

    init project for android and desktop

---
+++ android/src/siendev/corp/game/ashooter/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package siendev.corp.game.ashooter;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import siendev.corp.game.ashooter.Ashooter;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Ashooter(), config);
+	}
+}
---
+++ desktop/src/siendev/corp/game/ashooter/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package siendev.corp.game.ashooter.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import siendev.corp.game.ashooter.Ashooter;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Ashooter(), config);
+	}
+}
